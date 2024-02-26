85395fd25471dd4b216fc021c13dc97923b63c97
rafael peco <rafaelgamer321tosta@gmail.com>
2024-02-26 16:39:03 -0300

    a

---
+++ exjs/JS19/switch.js
@@ -0,0 +1,14 @@
+const resultado = prompt("escolha uma alternativa")
+
+const resultadoDoNumero = parseFloat(resultado)
+
+switch ("resultadoDoNumero") {
+    case "1":
+      alert("o resultado é 'a'")
+    case "2":
+      alert("o resultado é 'b'")
+    case "3":
+      alert("o resultado é 'c'")      
+    default:
+      alert("finalizando")    
+}
