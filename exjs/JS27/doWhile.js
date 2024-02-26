2aa66c1227d9c36b857541d7989f32ad999b309a
rafael peco <rafaelgamer321tosta@gmail.com>
2024-02-26 18:06:07 -0300

    a

---
+++ exjs/JS27/doWhile.js
@@ -0,0 +1,7 @@
+let velocidade = 0
+
+do {
+    alert("A velocidade do veículo é " + velocidade + " km/h")
+} while (velocidade > 0)
+
+alert ("Velocidade final: " + velocidade + " km/h")
---
+++ exjs/exercicio3/conversor.js
@@ -0,0 +1,34 @@
+const medida = prompt("Insira sua medida em metros:")
+
+const unidade = prompt (
+    "Para qual unidade de medida deseja converter?" +
+    "\n1 - milímetros (mm)" +
+    "\n2 - centímetros (cm)" +
+    "\n3 - decímetros (dm)" +
+    "\n4 - decâmetros(dam)" +
+    "\n5 - hectômetro (hm)" +
+    "\n6 - quilômetro (km)" 
+)
+
+switch (unidade) {
+    case "1":
+        alert("Resultad: " + medida + "m = " + medida * 1000 + "mm")
+        break
+    case "2":
+        alert("Resultad: " + medida + "m = " + medida * 100 + "cm")
+        break
+    case "3":
+        alert("Resultad: " + medida + "m = " + medida * 10 + "dm")
+        break
+    case "4":
+        alert("Resultad: " + medida + "m = " + medida / 10 + "dam")
+        break
+    case "5":
+        alert("Resultad: " + medida + "m = " + medida / 100 + "hm")
+        break
+    case "6":
+        alert("Resultad: " + medida + "m = " + medida / 1000 + "km")
+        break
+    default:
+        alert("Opção invalida.")
+}
