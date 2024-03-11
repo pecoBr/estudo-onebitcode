76a245f5e21e4a55c832afe0f7724f6fc29e32ab
rafael peco <rafaelgamer321tosta@gmail.com>
2024-03-11 20:31:29 -0300

    a

---
+++ exjs/MODULO2/aula14/retorno.js
@@ -0,0 +1,36 @@
+// Retorno é a saída da função
+
+// O retorno é usado para armazenarmos a saída da função em uma variável
+// Se não explicitarmos qual deve ser a saída da função, sua saída será undefined
+
+
+
+
+
+
+
+// Para dizer qual é a saída de uma função usamos o return
+
+
+
+
+// Também podemos jogar a saída de uma função dentro dos parâmetros de outra
+
+
+
+
+
+// E o retorno não precisa ser uma variável,
+// pode ser o resultado de qualquer expressão
+
+
+
+
+// Uma função só pode ter uma saída, depois do return nada é executado
+// Repare que o VS Code nos avisa que nosso código não será executado
+
+
+
+
+// No entanto, diferentes ramificações dentro
+// da função podem ter diferentes retornos
