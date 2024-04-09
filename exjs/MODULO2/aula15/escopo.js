3cf567209168416475b05ea4cde1dbc53263afd6
pecoBr <rafaelgamer321tosta@gmail.com>
2024-04-09 17:44:28 -0300

    exercícios da onebitcode

---
+++ exjs/MODULO2/aula15/escopo.js
@@ -0,0 +1,52 @@
+/*
+let pokemon = "Charmander"
+
+function evoluir() {
+    pokemon = "Charmileon"
+}
+ 
+console.log(pokemon)
+evoluir()
+console.log(pokemon)
+
+function criarAnimal() {
+    let animal = "Gato"
+}
+
+criarAnimal()
+console.log(animal)
+
+function avaliarNota(nota) {
+    if(nota > 60) {
+        var aprovado = true
+        let situacao = "Aprovado"
+    } else {
+        var aprovado = false
+        let situacao = "Reprovado"
+    }
+
+    console.log(nota)
+    console.log(aprovado)
+    console.log(situacao)
+}
+
+avaliarNota(85)
+avaliarNota(49)
+
+function ola() {
+    var texto = "Olá, mundo!"
+}
+
+console.log(texto)
+*/
+
+// Apenas variáveis declaradas com var são carregadas
+// sempre no começo do código, acima de todo o resto
+console.log(nome)
+//console.log(sobrenome)
+
+var nome = "Isaac"
+let sobrenome = "Pontes"
+
+console.log(nome)
+//console.log(sobrenome)
