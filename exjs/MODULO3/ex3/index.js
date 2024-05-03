be0df938edcfe16fe1e861c55d4d9a25d57578f2
pecoBr <rafaelgamer321tosta@gmail.com>
2024-05-02 21:50:40 -0300

    quase terminando o jogo da velha

--- exjs/MODULO3/ex3/index.js
+++ exjs/MODULO3/ex3/index.js
@@ -1,6 +1,7 @@
 // Variáveis globais úteis
-const boardRegions = document.querySelectorAll('#gameBoar span')
-const vBoard = []
+// Variáveis globais úteis
+const boardRegions = document.querySelectorAll('#gameBoard span')
+let vBoard = []
 let turnPlayer = ''
 
 function updateTitle() {
@@ -9,14 +10,46 @@ function updateTitle() {
 }
 
 function initializeGame() {
+  // Inicializa as variáveis globais 
   vBoard = [['', '', ''], ['', '', ''], ['', '', '']]
   turnPlayer = 'player1'
+  // Ajusta o título da página (caso seja necessário)
   document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
   updateTitle()
+  // Limpa o tabuleiro (caso seja necessário) e adiciona os eventos de clique
   boardRegions.forEach(function (element) {
     element.classList.remove('win')
     element.innerText = ''
+    element.classList.add('cursor-pointer')
     element.addEventListener('click', handleBoardClick)
   })
 }
 
+function disableRegion(element) {
+  element.style.cursor = 'default'
+  element.removeEventListener('click', handleBoardClick)  
+}
+
+function handleBoardClick(ev) {
+  // Obtém os índices da região clicada
+  const span = ev.currentTarget
+  const region = span.dataset.region // N.N
+  const rowColumnPair = region.split('.') // ["N", "N"]
+  const row = rowColumnPair[0]
+  const column = rowColumnPair[1]
+  // Marca a região clicada com o símbolo do jogador
+  if (turnPlayer === 'player1') {
+    span.innerText = 'X'
+    vBoard[row][column] = 'X'
+  } else {
+    span.innerText = 'O'
+    vBoard[row][column] = 'O'
+  }
+  // Limpa o console e exibe nosso tabuleiro virtual
+  console.clear()
+  console.table(vBoard)
+  // Desabilita a região clicada
+  disableRegion(span)
+}
+
+document.getElementById('start').addEventListener('click', initializeGame)
