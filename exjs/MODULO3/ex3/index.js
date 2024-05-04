4ef561b108396386afc1ab784dd53f745b101f4d
pecoBr <rafaelgamer321tosta@gmail.com>
2024-05-03 21:05:01 -0300

    quase terminando o modulo3

--- exjs/MODULO3/ex3/index.js
+++ exjs/MODULO3/ex3/index.js
@@ -25,9 +25,38 @@ function initializeGame() {
   })
 }
 
+function getWinRegions() {
+  const winRegions = []
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+    winRegions.push("0.0", "0.1", "0.2")
+  if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+    winRegions.push("1.0", "1.1", "1.2")
+  if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+    winRegions.push("2.0", "2.1", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+    winRegions.push("0.0", "1.0", "2.0")
+  if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+    winRegions.push("0.1", "1.1", "2.1")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+    winRegions.push("0.2", "1.2", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+    winRegions.push("0.0", "1.1", "2.2")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+    winRegions.push("0.2", "1.1", "2.0")
+  return winRegions
+}
+
 function disableRegion(element) {
-  element.style.cursor = 'default'
+  element.classList.remove('cursor-pointer')
   element.removeEventListener('click', handleBoardClick)  
+}
+
+function handleWin (regions) {
+  regions.forEach(function (region) {
+    document.querySelector('[data-region="' + region + '"]').classList.add('win')
+  })
+  const playerName = document.getElementById(turnPlayer).value
+  document.querySelector('h2').innerHTML = playerName + ' venceu!'
 }
 
 function handleBoardClick(ev) {
@@ -50,6 +79,15 @@ function handleBoardClick(ev) {
   console.table(vBoard)
   // Desabilita a região clicada
   disableRegion(span)
+  const winRegions = getWinRegions()
+  if (winRegions.length > 0) {
+    handleWin(winRegions)
+  } else if (vBoard.flat().includes('')) {
+    turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
+    updateTitle()
+  } else {
+    document.querySelector('h2').innerHTML = 'Empate!'
+  }
 }
-
+// Adiciona o evento no botão que inicia o jogo
 document.getElementById('start').addEventListener('click', initializeGame)
