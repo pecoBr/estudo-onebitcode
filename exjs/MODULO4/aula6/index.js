a2426369c4d4c3ecd0826f0735512749a9180c67
pecoBr <rafaelgamer321tosta@gmail.com>
2024-05-06 17:32:59 -0300

    mts estudos

---
+++ exjs/MODULO4/aula6/index.js
@@ -0,0 +1,28 @@
+const person = {
+  name: "Luke",
+  job: "Farmer",
+  parents: ["Anakin", "Padme"]
+}
+
+const name = person.name
+
+const { job, parents } = person
+
+console.log(name, job, parents)
+
+const [father, mother] = parents
+
+console.log(father, mother)
+
+function creatUser ({ name, job, parents }) {
+  const id = Math.floor(Math.random() * 9999)
+  return {
+    id,
+    name,
+    job,
+    parents
+  }
+}
+  const luke = creatUser(person)
+
+  console.log(luke)
